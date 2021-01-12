58c511702753e8fbb151c68c339063db8dc7f00d
Nur Alam Zico <nur.zico@tigeritbd.com>
2021-01-12 19:00:15 +0600

    package updated to the new repo

--- server.go
+++ server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/handlers"
-	"github.com/gvalkov/tailon/cmd"
-	"github.com/gvalkov/tailon/frontend"
+	"github.com/nurzico/tailon/cmd"
+	"github.com/nurzico/tailon/frontend"
 	"github.com/shurcooL/httpfs/html/vfstemplate"
 	"github.com/shurcooL/httpgzip"
 	"gopkg.in/igm/sockjs-go.v2/sockjs"
